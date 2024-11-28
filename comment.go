5bc4281f3da4370deb84d14ee6780ec30f600cc5
aeddi <antoine.e.b@gmail.com>
2024-11-28 15:34:07 +0900

    test

--- comment.go
+++ comment.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"bot/client"
 	"bytes"
+	"errors"
 	"fmt"
-	"os"
 	"regexp"
 	"strings"
 	"text/template"
 
-	"github.com/google/go-github/v66/github"
+	"github.com/gnolang/gno/contribs/github-bot/internal/client"
+	"github.com/gnolang/gno/contribs/github-bot/internal/utils"
+
+	"github.com/google/go-github/v64/github"
 	"github.com/sethvargo/go-githubactions"
 )
 
+var errTriggeredByBot = errors.New("event triggered by bot")
+
+// Compile regex only once.
+var (
+	// Regex for capturing the entire line of a manual check.
+	manualCheckLine = regexp.MustCompile(`(?m:^-\s\[([ xX])\]\s+(.+?)\s*(\(checked by @(\w+)\))?$)`)
+	// Regex for capturing only the checkboxes.
+	checkboxes = regexp.MustCompile(`(?m:^- \[[ x]\])`)
+	// Regex used to capture markdown links.
+	markdownLink = regexp.MustCompile(`\[(.*)\]\(.*\)`)
+)
+
 // These structures contain the necessary information to generate
-// the bot's comment from the template file
+// the bot's comment from the template file.
 type AutoContent struct {
 	Description        string
 	Satisfied          bool
@@ -23,256 +37,231 @@ type AutoContent struct {
 }
 type ManualContent struct {
 	Description      string
-	ConditionDetails string
 	CheckedBy        string
+	ConditionDetails string
 	Teams            []string
 }
 type CommentContent struct {
-	AutoRules   []AutoContent
-	ManualRules []ManualContent
+	AutoRules    []AutoContent
+	ManualRules  []ManualContent
+	allSatisfied bool
 }
 
-// getCommentManualChecks parses the bot comment to get both the checkbox status,
-// the check description and the username who checked it
-func getCommentManualChecks(commentBody string) map[string][2]string {
-	checks := make(map[string][2]string)
+type manualCheckDetails struct {
+	status    string
+	checkedBy string
+}
 
-	reg := regexp.MustCompile(`(?m:^- \[([ x])\] (.+)?$)`)
-	subReg := regexp.MustCompile(`(?m:(.+) \(checked by @(\w+)\)$)`)
-	matches := reg.FindAllStringSubmatch(commentBody, -1)
+// getCommentManualChecks parses the bot comment to get the checkbox status,
+// the check description and the username who checked it.
+func getCommentManualChecks(commentBody string) map[string]manualCheckDetails {
+	checks := make(map[string]manualCheckDetails)
 
-	for _, match := range matches {
-		if subMatches := subReg.FindAllStringSubmatch(match[2], -1); len(subMatches) > 0 {
-			checks[subMatches[0][1]] = [2]string{match[1], subMatches[0][2]}
-		} else {
-			checks[match[2]] = [2]string{match[1]}
+	// For each line that matches the "Manual check" regex.
+	for _, match := range manualCheckLine.FindAllStringSubmatch(commentBody, -1) {
+		description := match[2]
+		status := match[1]
+		checkedBy := ""
+		if len(match) > 4 {
+			checkedBy = strings.ToLower(match[4]) // if X captured, convert it to x.
 		}
+
+		checks[description] = manualCheckDetails{status: status, checkedBy: checkedBy}
 	}
 
 	return checks
 }
 
-// handleCommentUpdate checks if :
+// handleCommentUpdate checks if:
 //   - the current run was triggered by GitHub Actions
 //   - the triggering event is an edit of the bot comment
 //   - the comment was not edited by the bot itself (prevent infinite loop)
 //   - the comment change is only a checkbox being checked or unckecked (or restore it)
 //   - the actor / comment editor has permission to modify this checkbox (or restore it)
-func handleCommentUpdate(gh *client.GitHub) {
-	// Get GitHub Actions context to retrieve comment update
-	actionCtx, err := githubactions.Context()
-	if err != nil {
-		gh.Logger.Debugf("Unable to retrieve GitHub Actions context : %v", err)
-		return
+func handleCommentUpdate(gh *client.GitHub, actionCtx *githubactions.GitHubContext) error {
+	// Ignore if it's not a comment related event.
+	if actionCtx.EventName != utils.EventIssueComment {
+		gh.Logger.Debugf("Event is not issue comment related (%s)", actionCtx.EventName)
+		return nil
 	}
 
-	// Ignore if it's not an comment related event
-	if actionCtx.EventName != "issue_comment" {
-		gh.Logger.Debugf("Event is not issue comment related : %s", actionCtx.EventName)
-		return
-	}
-
-	// Ignore if action type is not deleted or edited
+	// Ignore if the action type is not deleted or edited.
 	actionType, ok := actionCtx.Event["action"].(string)
 	if !ok {
-		gh.Logger.Errorf("Unable to get type on issue comment event")
-		os.Exit(1)
+		return errors.New("unable to get type on issue comment event")
 	}
 
 	if actionType != "deleted" && actionType != "edited" {
-		return
+		return nil
 	}
 
-	// Exit if comment was edited by bot (current authenticated user)
+	// Return if comment was edited by bot (current authenticated user).
 	authUser, _, err := gh.Client.Users.Get(gh.Ctx, "")
 	if err != nil {
-		gh.Logger.Errorf("Unable to get authenticated user : %v", err)
-		os.Exit(1)
+		return fmt.Errorf("unable to get authenticated user: %w", err)
 	}
 
 	if actionCtx.Actor == authUser.GetLogin() {
 		gh.Logger.Debugf("Prevent infinite loop if the bot comment was edited by the bot itself")
-		os.Exit(0)
+		return errTriggeredByBot
 	}
 
-	// Ignore if edited comment author is not the bot
-	comment, ok := actionCtx.Event["comment"].(map[string]any)
+	// Get login of the author of the edited comment.
+	login, ok := utils.IndexMap(actionCtx.Event, "comment", "user", "login").(string)
 	if !ok {
-		gh.Logger.Errorf("Unable to get comment on issue comment event")
-		os.Exit(1)
+		return errors.New("unable to get comment user login on issue comment event")
 	}
 
-	author, ok := comment["user"].(map[string]any)
-	if !ok {
-		gh.Logger.Errorf("Unable to get comment user on issue comment event")
-		os.Exit(1)
-	}
-
-	login, ok := author["login"].(string)
-	if !ok {
-		gh.Logger.Errorf("Unable to get comment user login on issue comment event")
-		os.Exit(1)
-	}
-
+	// If the author is not the bot, return.
 	if login != authUser.GetLogin() {
-		return
+		return nil
 	}
 
-	// Get comment current body
-	current, ok := comment["body"].(string)
+	// Get comment updated body.
+	current, ok := utils.IndexMap(actionCtx.Event, "comment", "body").(string)
 	if !ok {
-		gh.Logger.Errorf("Unable to get comment body on issue comment event")
-		os.Exit(1)
+		return errors.New("unable to get comment body on issue comment event")
 	}
 
-	// Get comment updated body
-	changes, ok := actionCtx.Event["changes"].(map[string]any)
+	// Get comment previous body.
+	previous, ok := utils.IndexMap(actionCtx.Event, "changes", "body", "from").(string)
 	if !ok {
-		gh.Logger.Errorf("Unable to get changes on issue comment event")
-		os.Exit(1)
+		return errors.New("unable to get changes body content on issue comment event")
 	}
 
-	changesBody, ok := changes["body"].(map[string]any)
-	if !ok {
-		gh.Logger.Errorf("Unable to get changes body on issue comment event")
-		os.Exit(1)
+	// Get PR number from GitHub Actions context.
+	prNum, ok := utils.IndexMap(actionCtx.Event, "issue", "number").(float64)
+	if !ok || prNum <= 0 {
+		return errors.New("unable to get issue number on issue comment event")
 	}
 
-	previous, ok := changesBody["from"].(string)
-	if !ok {
-		gh.Logger.Errorf("Unable to get changes body content on issue comment event")
-		os.Exit(1)
-	}
-
-	// Get PR number from GitHub Actions context
-	issue, ok := actionCtx.Event["issue"].(map[string]any)
-	if !ok {
-		gh.Logger.Errorf("Unable to get issue on issue comment event")
-		os.Exit(1)
-	}
-
-	num, ok := issue["number"].(float64)
-	if !ok || num <= 0 {
-		gh.Logger.Errorf("Unable to get issue number on issue comment event")
-		os.Exit(1)
-	}
-
-	// Check if change is only a checkbox being checked or unckecked
-	checkboxes := regexp.MustCompile(`(?m:^- \[[ x]\])`)
+	// Check if change is only a checkbox being checked or unckecked.
 	if checkboxes.ReplaceAllString(current, "") != checkboxes.ReplaceAllString(previous, "") {
-		// If not, restore previous comment body
-		gh.Logger.Errorf("Bot comment edited outside of checkboxes")
-		gh.SetBotComment(previous, int(num))
-		os.Exit(1)
+		// If not, restore previous comment body.
+		if !gh.DryRun {
+			gh.SetBotComment(previous, int(prNum))
+		}
+		return errors.New("bot comment edited outside of checkboxes")
 	}
 
-	// Check if actor / comment editor has permission to modify changed boxes
+	// Check if actor / comment editor has permission to modify changed boxes.
 	currentChecks := getCommentManualChecks(current)
 	previousChecks := getCommentManualChecks(previous)
 	edited := ""
 	for key := range currentChecks {
-		if currentChecks[key][0] != previousChecks[key][0] {
-			// Get teams allowed to edit this box from config
-			var teams []string
-			found := false
-			_, manualRules := config(gh)
+		// If there is no diff for this check, ignore it.
+		if currentChecks[key].status == previousChecks[key].status {
+			continue
+		}
 
-			for _, manualRule := range manualRules {
-				if manualRule.Description == key {
-					found = true
-					teams = manualRule.Teams
+		// Get teams allowed to edit this box from config.
+		var teams []string
+		found := false
+		_, manualRules := config(gh)
+
+		for _, manualRule := range manualRules {
+			if manualRule.description == key {
+				found = true
+				teams = manualRule.teams
+			}
+		}
+
+		// If rule were not found, return to reprocess the bot comment entirely
+		// (maybe bot config was updated since last run?).
+		if !found {
+			gh.Logger.Debugf("Updated rule not found in config: %s", key)
+			return nil
+		}
+
+		// If teams specified in rule, check if actor is a member of one of them.
+		if len(teams) > 0 {
+			if gh.IsUserInTeams(actionCtx.Actor, teams) {
+				if !gh.DryRun {
+					gh.SetBotComment(previous, int(prNum))
 				}
+				return errors.New("checkbox edited by a user not allowed to")
 			}
+		}
 
-			// If rule were not found, return to reprocess the bot comment entirely
-			// (maybe bot config was updated since last run?)
-			if !found {
-				gh.Logger.Debugf("Updated rule not found in config : %s", key)
-				return
-			}
+		// This regex capture only the line of the current check.
+		specificManualCheck := regexp.MustCompile(fmt.Sprintf(`(?m:^- \[%s\] %s.*$)`, currentChecks[key].status, regexp.QuoteMeta(key)))
 
-			// If teams specified in rule, check if actor is a member of one of them
-			if len(teams) > 0 {
-				if gh.IsUserInTeams(actionCtx.Actor, teams) {
-					gh.Logger.Errorf("Checkbox edited by a user not allowed to")
-					gh.SetBotComment(previous, int(num))
-					os.Exit(1)
-				}
-			}
-
-			// If box was checked
-			reg := regexp.MustCompile(fmt.Sprintf(`(?m:^- \[%s\] %s.*$)`, currentChecks[key][0], key))
-			if strings.TrimSpace(currentChecks[key][0]) == "x" {
-				replacement := fmt.Sprintf("- [%s] %s (checked by @%s)", currentChecks[key][0], key, actionCtx.Actor)
-				edited = reg.ReplaceAllString(current, replacement)
-			} else {
-				replacement := fmt.Sprintf("- [%s] %s", currentChecks[key][0], key)
-				edited = reg.ReplaceAllString(current, replacement)
-			}
+		// If the box is checked, append the username of the user who checked it.
+		if strings.TrimSpace(currentChecks[key].status) == "x" {
+			replacement := fmt.Sprintf("- [%s] %s (checked by @%s)", currentChecks[key].status, key, actionCtx.Actor)
+			edited = specificManualCheck.ReplaceAllString(current, replacement)
+		} else {
+			// Else, remove the username of the user.
+			replacement := fmt.Sprintf("- [%s] %s", currentChecks[key].status, key)
+			edited = specificManualCheck.ReplaceAllString(current, replacement)
 		}
 	}
 
-	// Update comment with username
-	if edited != "" {
-		gh.SetBotComment(edited, int(num))
-		gh.Logger.Debugf("Comment manual checks updated successfuly")
+	// Update comment with username.
+	if edited != "" && !gh.DryRun {
+		gh.SetBotComment(edited, int(prNum))
+		gh.Logger.Debugf("Comment manual checks updated successfully")
 	}
+
+	return nil
 }
 
-func updateComment(gh *client.GitHub, pr *github.PullRequest, content CommentContent) {
-	// Custom function to string markdown links
+// generateComment generates a comment using the template file and the
+// content passed as parameter.
+func generateComment(content CommentContent) (string, error) {
+	// Custom function to strip markdown links.
 	funcMap := template.FuncMap{
 		"stripLinks": func(input string) string {
-			reg := regexp.MustCompile(`\[(.*)\]\(.*\)`)
-			return reg.ReplaceAllString(input, "$1")
+			return markdownLink.ReplaceAllString(input, "$1")
 		},
 	}
 
-	// Generate bot comment using template file
+	// Bind markdown stripping function to template generator.
 	const tmplFile = "comment.tmpl"
 	tmpl, err := template.New(tmplFile).Funcs(funcMap).ParseFiles(tmplFile)
 	if err != nil {
-		panic(err)
+		return "", fmt.Errorf("unable to init template: %w", err)
 	}
 
+	// Generate bot comment using template file.
 	var commentBytes bytes.Buffer
 	if err := tmpl.Execute(&commentBytes, content); err != nil {
-		panic(err)
+		return "", fmt.Errorf("unable to execute template: %w", err)
 	}
 
-	// Prepare commit status content
+	return commentBytes.String(), nil
+}
+
+// updatePullRequest updates or creates both the bot comment and the commit status.
+func updatePullRequest(gh *client.GitHub, pr *github.PullRequest, content CommentContent) error {
+	// Generate comment text content.
+	commentText, err := generateComment(content)
+	if err != nil {
+		return fmt.Errorf("unable to generate comment on PR %d: %w", pr.GetNumber(), err)
+	}
+
+	// Update comment on pull request.
+	comment, err := gh.SetBotComment(commentText, pr.GetNumber())
+	if err != nil {
+		return fmt.Errorf("unable to update comment on PR %d: %w", pr.GetNumber(), err)
+	} else {
+		gh.Logger.Infof("Comment successfully updated on PR %d", pr.GetNumber())
+	}
+
+	// Prepare commit status content.
 	var (
-		comment      = gh.SetBotComment(commentBytes.String(), pr.GetNumber())
-		context      = "Merge Requirements"
-		targetURL    = comment.GetHTMLURL()
-		state        = "pending"
-		description  = "Some requirements are not satisfied yet. See bot comment."
-		allSatisfied = true
+		context     = "Merge Requirements"
+		targetURL   = comment.GetHTMLURL()
+		state       = "failure"
+		description = "Some requirements are not satisfied yet. See bot comment."
 	)
 
-	// Check if every requirements are satisfied
-	for _, auto := range content.AutoRules {
-		if !auto.Satisfied {
-			allSatisfied = false
-			break
-		}
-	}
-
-	if allSatisfied {
-		for _, manual := range content.ManualRules {
-			if manual.CheckedBy == "" {
-				allSatisfied = false
-				break
-			}
-		}
-	}
-
-	if allSatisfied {
+	if content.allSatisfied {
 		state = "success"
 		description = "All requirements are satisfied."
 	}
 
-	// Update or create commit status
+	// Update or create commit status.
 	if _, _, err := gh.Client.Repositories.CreateStatus(
 		gh.Ctx,
 		gh.Owner,
@@ -284,6 +273,10 @@ func updateComment(gh *client.GitHub, pr *github.PullRequest, content CommentCon
 			TargetURL:   &targetURL,
 			Description: &description,
 		}); err != nil {
-		gh.Logger.Errorf("Unable to create status on PR %d : %v", pr.GetNumber(), err)
+		return fmt.Errorf("unable to create status on PR %d: %w", pr.GetNumber(), err)
+	} else {
+		gh.Logger.Infof("Commit status successfully updated on PR %d", pr.GetNumber())
 	}
+
+	return nil
 }
--- config.go
+++ config.go
@@ -1,124 +1,80 @@
 package main
 
 import (
-	"bot/client"
-	c "bot/condition"
-	r "bot/requirement"
+	"github.com/gnolang/gno/contribs/github-bot/internal/client"
+	c "github.com/gnolang/gno/contribs/github-bot/internal/conditions"
+	r "github.com/gnolang/gno/contribs/github-bot/internal/requirements"
 )
 
+type Teams []string
+
+// Automatic check that will be performed by the bot.
 type automaticCheck struct {
-	Description string
-	If          c.Condition
-	Then        r.Requirement
+	description string
+	ifC         c.Condition   // If the condition is met, the rule is displayed and the requirement is executed.
+	thenR       r.Requirement // If the requirement is satisfied, the check passes.
 }
 
+// Manual check that will be performed by users.
 type manualCheck struct {
-	Description string
-	If          c.Condition
-	Teams       []string
+	description string
+	ifC         c.Condition // If the condition is met, a checkbox will be displayed on bot comment.
+	teams       Teams       // Members of these teams can check the checkbox to make the check pass.
 }
 
+// This function returns the configuration of the bot consisting of automatic and manual checks
+// in which the GitHub client is injected.
 func config(gh *client.GitHub) ([]automaticCheck, []manualCheck) {
 	auto := []automaticCheck{
 		{
-			Description: "Changes on 'foo' file should be reviewed by at least one [Bot PR](https://github.com/gnolang/gno/pull/3037) reviewer",
-			If: c.And(
-				c.BaseBranch("main"),
-				c.Or(
-					c.FileChanged(gh, "foo"),
-					c.FileChanged(gh, "bar"),
-					c.FileChanged(gh, "baz"),
-				),
+			description: "Maintainers must be able to edit this pull request",
+			ifC:         c.Always(),
+			thenR:       r.MaintainerCanModify(),
+		},
+		{
+			description: "The pull request head branch must be up-to-date with its base",
+			ifC:         c.Always(),
+			thenR:       r.UpToDateWith(gh, r.PR_BASE),
+		},
+		{
+			description: "Changes to 'docs' folder must be reviewed/authored by at least one devrel",
+			ifC:         c.FileChanged(gh, "docs"),
+			thenR: r.Or(
+				r.AuthorInTeam(gh, "devrels"),
+				r.ReviewByTeamMembers(gh, "devrels", 1),
 			),
-			Then: r.And(
-				r.Author("aeddi"),
-				r.Or(
-					r.ReviewByUser(gh, "thehowl"),
-					r.ReviewByUser(gh, "ltzmaxwell"),
-					r.ReviewByUser(gh, "zivkovicmilos"),
-				),
-			),
-		},
-		{
-			Description: "Maintainer must be able to edit this pull request",
-			If:          c.Always(),
-			Then:        r.MaintainerCanModify(),
-		},
-		{
-			Description: "Pull request head branch must be up to date with its base",
-			If:          c.Always(), // Or only if c.BaseBranch("main") ?
-			Then:        r.UpToDateWith(gh, r.PR_BASE),
-		},
-		{
-			Description: "Label bug is applied for no other reason than testing",
-			If:          c.HeadBranch("demo-pr"),
-			Then:        r.Label(gh, "bug"),
 		},
 	}
 
 	manual := []manualCheck{
 		{
-			Description: "Demo manual check",
-			If:          c.Always(),
+			description: "The pull request description provides enough details",
+			ifC:         c.Not(c.AuthorInTeam(gh, "core-contributors")),
+			teams:       Teams{"core-contributors"},
 		},
 		{
-			Description: "Demo manual check with lot of (useless) details",
-			If: c.Or(
-				c.Always(),
-				c.And(
-					c.Label("bug"),
-					c.BaseBranch("main"),
-					c.Or(
-						c.FileChanged(gh, "misc/deployments"),
-						c.FileChanged(gh, `misc/docker-\.*`),
-						c.FileChanged(gh, "tm2/pkg/p2p"),
-						c.FileChanged(gh, "contribs"),
-					),
-				),
-			),
-			Teams: []string{"foo", "bar", "baz"},
-		},
-		{
-			Description: "Determine if infra needs to be updated",
-			If: c.And(
-				c.BaseBranch("main"),
+			description: "Determine if infra needs to be updated before merging",
+			ifC: c.And(
+				c.BaseBranch("master"),
 				c.Or(
+					c.FileChanged(gh, `Dockerfile\.*`),
 					c.FileChanged(gh, "misc/deployments"),
 					c.FileChanged(gh, `misc/docker-\.*`),
-					c.FileChanged(gh, "tm2/pkg/p2p"),
+					c.FileChanged(gh, `.github/workflows/releaser\.*.yml`),
+					c.FileChanged(gh, `.github/workflows/portal-loop.yml`),
 				),
 			),
-			Teams: []string{"tech-staff"},
-		},
-		{
-			Description: "The code style is satisfactory",
-			If: c.And(
-				c.BaseBranch("main"),
-				c.Or(
-					c.FileChanged(gh, `.*\.go`),
-					c.FileChanged(gh, `.*\.js`),
-				),
-			),
-			Teams: []string{"tech-staff"},
-		},
-		{
-			Description: "The documentation is accurate and relevant",
-			If:          c.FileChanged(gh, `.*\.md`),
-			Teams: []string{
-				"tech-staff",
-				"devrels",
-			},
+			teams: Teams{"devops"},
 		},
 	}
 
-	// Check for duplicates in manual rule descriptions
-	// (need to be unique for the bot operations)
+	// Check for duplicates in manual rule descriptions (needs to be unique for the bot operations).
 	unique := make(map[string]struct{})
 	for _, rule := range manual {
-		if _, exists := unique[rule.Description]; exists {
-			gh.Logger.Fatalf("Manual rule description must be unique (duplicate : %s)", rule.Description)
+		if _, exists := unique[rule.description]; exists {
+			gh.Logger.Fatalf("Manual rule descriptions must be unique (duplicate: %s)", rule.description)
 		}
-		unique[rule.Description] = struct{}{}
+		unique[rule.description] = struct{}{}
 	}
 
 	return auto, manual
